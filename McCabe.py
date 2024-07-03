83c39ed230627aee6f8dd89b3b0404be7d508e28
Victor Liang <112746859+Victor-Liang-ChE@users.noreply.github.com>
2024-07-03 06:59:27 -0700

    Add files via upload

--- McCabe.py
+++ McCabe.py
@@ -6,10 +6,14 @@ from scipy.optimize import fsolve
 import warnings
 from numpy import RankWarning, VisibleDeprecationWarning
 
-def mccabe(comp1, comp2, xd, xb, xf = 0.5, P = None, T = None, steps = True, pointson = True):
-    # Suppress RankWarning
+def mccabe(comp1, comp2, xd, xb, xf = 0.5, P = None, T = None, R = None, q = None, stages = True, feedstage = True, pointson = True):
+
+    # Suppress Errors
     warnings.simplefilter('ignore', RankWarning)
     warnings.simplefilter('ignore', VisibleDeprecationWarning)
+    warnings.simplefilter('ignore', RuntimeWarning)
+
+    # Check if P or T is given
     if P is None and T is None:
         print('Please provide either a temperature or a pressure')
         return None
@@ -19,47 +23,142 @@ def mccabe(comp1, comp2, xd, xb, xf = 0.5, P = None, T = None, steps = True, poi
     elif T is not None: # elif is necessary here since T is define right before this
         P = 1e5 # Pa
         Pgiven = False
+
+    #Check for q and R value condition
+    if q is not None and R is not None:
+        if q == 1: # this will make the slopes infinity
+            feedslope = 1e10
+        else:
+            feedslope = q/(q-1)
+        if R == -1: # this will make the slopes infinity
+            rectifyslope = 1e10
+        else:
+            rectifyslope = R/(R + 1)
+        if feedslope == rectifyslope:
+            print('Please provide a valid q and R value')
+            return None
+
+    # Get the equilibrium data
     xi, yi = xy(comp1, comp2, P = P, values = True, show = False)
-    # fit a curve to the data
+
+    # Fit a curve to the data
     z = np.polyfit(xi, yi, 30)
     p = np.poly1d(z)
+
+    # Initialize the plot
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plt.plot(xi, p(xi))
     plt.plot(xi, xi)
-    steps = 0
-    x = xd
-    while x > xb:
-        def difference(xval):
-            return p(xval) - x
-        intersect = fsolve(difference, x)
-        if intersect > x or intersect == x:
-            print('Cannot perform McCabe-Thiele Method as equilibrium curve is below y=x at distillation composition')
-            break
-        # draw a horizontal line from xd,xd to the best fit curve
-        plt.plot([x, intersect], [x,x], ls = '-', color = 'black') # ([initialx, finalx],[initialy, finaly])
-        plt.plot([intersect, intersect], [x, intersect], ls = '-', color = 'black') # ([initialx, finalx],[initialy, finaly])
-        x = intersect
-        steps += 1
-    #annotate at the bottom right th enumber of steps
-    plt.annotate(f'steps = {steps}', xy=(0.8, 0.1), xycoords='axes fraction', fontsize=12, ha='center', va='center')
+
+    # Define and plot the feed, rectifying, and stripping lines and intersection
+    if q is not None and R is not None:
+        def rectifying(xval):
+            return R/(R+1)*xval + xd/(R+1)
+        def feed(xval):
+            return q/(q-1)*xval - xf/(q-1)
+        def feedrectintersection(xval):
+            return q/(q-1)*xval - xf/(q-1) - R/(R+1)*xval - xd/(R+1)
+        xguess = xf*q # q>1 means xsol > xf, and q<1 means xsol < xf
+        if R == -1:
+            xsol = xd
+            ysol = feed(xsol)
+        elif q == 1:
+            xsol = xf
+            ysol = rectifying(xsol)
+        else:
+            xsol = fsolve(feedrectintersection, xguess)  # fsolve returns an array, get the first element
+            ysol = rectifying(xsol)
+        # define the stripping line as the line from xb to the intersection of the rectifying line and the feed line
+        def stripping(xval):
+            return (ysol-xb)*(xval-xb)/(xsol-xb)+xb
+        plt.plot([xf, xsol], [xf, ysol], ls=':', color='black', label='Feed to Rectifying Line')
+        plt.plot([xd, xsol], [xd, ysol], ls='--', color='black', label='Distillate to Feed Line')
+        plt.plot([xb, xsol], [xb, ysol], ls='-.', color='black', label='Bottom to Feed Line')
+
+    # Perform Step-off process for no feed condition
+    stages = 0 # initialize the number of stages
+    x = xd # start off at the distillation composition
+    if q is None and R is None:
+        while x > xb:
+            def difference(xval):
+                return p(xval) - x
+            intersect = fsolve(difference, 0)
+            if intersect > x or intersect == x:
+                print('Cannot perform McCabe-Thiele Method as equilibrium curve is below y=x at distillation composition')
+                break
+            # draw a horizontal line from y=x to the best fit curve
+            plt.plot([x, intersect], [x,x], ls = '-', color = 'black') # ([initialx, finalx],[initialy, finaly])
+            plt.plot([intersect, intersect], [x, intersect], ls = '-', color = 'black') # ([initialx, finalx],[initialy, finaly])
+            x = intersect
+            stages += 1
+
+    # Perform step off for feed condition
+    else:
+        y = xd
+        feedstage = 1
+        while x > xb:
+            def difference(xval):
+                return p(xval) - y
+            intersect = fsolve(difference, 0)
+            if intersect > x or intersect == x:
+                print('Cannot perform McCabe-Thiele Method as equilibrium curve is below y=x at distillation composition')
+                break
+            plt.plot([x, intersect], [y,y], ls = '-', color = 'black')
+            if intersect > xsol:
+                # draw a horizontal line from rectifying line to the best fit curve 
+                plt.plot([intersect, intersect], [y, rectifying(intersect)], ls = '-', color = 'black')
+                x = intersect
+                y = rectifying(intersect)
+                stages += 1
+                feedstage += 1
+            else:
+                # draw a horizontal line from stripping line to the best fit curve 
+                plt.plot([intersect, intersect], [y, stripping(intersect)], ls = '-', color = 'black')
+                x = intersect
+                y = stripping(intersect)
+                stages += 1
+
+    # Add labels to the plot
     plt.xlabel(f'Liquid mole fraction {comp1}', fontsize = 16)
     plt.ylabel(f'Vapor mole fraction {comp1}', fontsize = 16)
     plt.title(f'McCabe-Thiele Method for {comp1} + {comp2}', fontsize = 16)
+
+    # Allow for points to be shown
     if pointson:
-        #show where the xd and xb are 
         plt.plot(xd, xd, 'ro')
         plt.plot(xb, xb, 'ro')
-        #label each point
-        ax.annotate('xd', (xd, xd), textcoords="offset points", xytext=(0,-20), ha='center')
-        ax.annotate('xb', (xb, xb), textcoords="offset points", xytext=(0,-20), ha='center')
+        plt.plot(xf, xf, 'ro')
+        ax.annotate('xd', (xd, xd), textcoords="offset points", xytext=(0,-15), ha='center')
+        if xb < 0.05:
+            ax.annotate('xb', (xb, xb), textcoords="offset points", xytext=(15,-3), ha='center')
+        else:
+            ax.annotate('xb', (xb, xb), textcoords="offset points", xytext=(0,-15), ha='center')
+        ax.annotate('xf', (xf, xf), textcoords="offset points", xytext=(0,-15), ha='center')
         ax.tick_params(labelsize = 14)
         ax.set_xlim(0,1)
         ax.set_ylim(0,1)
+
+    # Creating a nice output boxes
+    textstr1 = '\n'.join((
+        r'$Input:$',
+        r'$x_d=%.2f$' % (xd, ),
+        r'$x_f=%.2f$' % (xf, ),
+        r'$x_b=%.2f$' % (xb, ),
+        r'$q=%.2f$' % (q, ),
+        r'$R=%.2f$' % (R, )))
+    textstr2 = '\n'.join((
+        r'$Output:$',
+        r'$Stages=%.0f$' % (stages, ),
+        r'$Feed \:Stage=%.0f$' % (feedstage, )))
+    # place a text box in upper left in axes coords
+    props = dict(boxstyle='round', facecolor='green', alpha=0.2)
+    plt.text(0.55, 0.05, textstr1, fontsize=10, verticalalignment='bottom', bbox=props)
+    plt.text(0.75, 0.05, textstr2, fontsize=10, verticalalignment='bottom', bbox=props)
+
+    # Show the plot
     plt.show()
-    if steps:
-        print(f'Steps = {steps}')
 
 # %%
-mccabe('methanol', 'water', xd = 0.95, xb = 0.1, P = 1)
+#mccabe('methanol', 'water', xd = 0.95, xb = 0.01, P = 1, q = 1, R = 3)
 # %%
